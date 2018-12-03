09bdb256b69627732706a80250d4db9dc2ee82fb
majki <michofilip@gmail.com>
2018-12-03 22:03:45 +0100

    replace repo with service

--- server/src/main/java/com/example/OperationDB/controllers/OperationController.java
+++ server/src/main/java/com/example/OperationDB/controllers/OperationController.java
@@ -1,36 +1,33 @@
 package com.example.OperationDB.controllers;
 
 import com.example.OperationDB.model.Operation;
-import com.example.OperationDB.parser.XMLFileParser;
-import com.example.OperationDB.repository.OperationRepository;
+import com.example.OperationDB.service.OperationService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Collection;
-import java.util.List;
 
 @RestController
 @RequestMapping("/operations")
 @AllArgsConstructor
 public class OperationController {
 
-    private OperationRepository operationRepository;
+    private OperationService operationService;
 
     @GetMapping("/")
     public Collection<Operation> getAll() {
-        return operationRepository.findAll();
+        return operationService.findAll();
     }
 
     @GetMapping("/{id}")
     public Operation getOne(@PathVariable Long id) {
-        return operationRepository.findOne(id);
+        return operationService.findOne(id);
     }
 
     @PostMapping("/")
     public void createFromFile(MultipartFile file) {
-        List<Operation> operations = XMLFileParser.parseFromXMLFile(file);
-        operationRepository.save(operations);
+        operationService.saveFromXMLFile(file);
     }
 
 }
