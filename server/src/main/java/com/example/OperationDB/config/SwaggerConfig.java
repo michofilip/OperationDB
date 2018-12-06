d934d141eedc4095df1626e2e39244263837c7f7
majki <michofilip@gmail.com>
2018-12-06 12:13:08 +0100

    swagger

---
+++ server/src/main/java/com/example/OperationDB/config/SwaggerConfig.java
@@ -0,0 +1,44 @@
+package com.example.OperationDB.config;
+
+import com.example.OperationDB.controllers.OperationController;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@PropertySource("classpath:swagger.properties")
+@ComponentScan(basePackageClasses = OperationController.class)
+@Configuration
+public class SwaggerConfig {
+    private final static String SWAGGER_API_VERSION = "1.0";
+    private final static String LICENSE_TEXT = "License";
+    private final static String TITLE = "Operations REST API";
+    private final static String DESCRIPTION = "RESTful api for operations";
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title(TITLE)
+                .description(DESCRIPTION)
+                .license(LICENSE_TEXT)
+                .version(SWAGGER_API_VERSION)
+                .build();
+    }
+
+    @Bean
+    public Docket operationsApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .pathMapping("/")
+                .select()
+                .paths(PathSelectors.regex("/.*"))
+                .build();
+    }
+}
+
--- server/src/main/java/com/example/OperationDB/controllers/OperationController.java
+++ server/src/main/java/com/example/OperationDB/controllers/OperationController.java
@@ -2,7 +2,12 @@ package com.example.OperationDB.controllers;
 
 import com.example.OperationDB.model.Operation;
 import com.example.OperationDB.service.OperationService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.AllArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -10,17 +15,22 @@ import java.util.Collection;
 
 @RestController
 @RequestMapping("/operations")
+@Api(value = "OperationControllerAPI", produces = MediaType.APPLICATION_JSON_VALUE)
 @AllArgsConstructor
 public class OperationController {
 
     private OperationService operationService;
 
     @GetMapping
+//    @ApiOperation("Gets operations")
+//    @ApiResponses(value = {@ApiResponse(code = 200, message = "OK", response = Collection.class)})
     public Collection<Operation> getAll() {
         return operationService.findAll();
     }
 
     @GetMapping("/{id}")
+    @ApiOperation("Gets operation by id")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "OK", response = Operation.class)})
     public Operation getOne(@PathVariable Long id) {
         return operationService.findOne(id);
     }
